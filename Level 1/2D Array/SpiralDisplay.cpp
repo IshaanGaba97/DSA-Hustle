5a1b5622ad235bce8808fe364441a59ddb7f563a
Ishaan Gaba <ishaangaba97@gmail.com>
2021-10-11 13:12:18 +0530

    added

--- Level 1/2D Array/SpiralDisplay.cpp
+++ Level 1/2D Array/SpiralDisplay.cpp
@@ -20,10 +20,45 @@ int main()
         }
     }
 
-    for(int j = 0; j < c; j++)
+    int minr = 0;
+    int minc = 0;
+    int maxr = r-1;
+    int maxc = c-1;
+    int tne = r * c;
+    int cnt = 0;
+    while (cnt < tne)
     {
-        for(int i = 0; i < r; i++)
+        // left wall 
+        for (int i = minr,  j = minc; i <= maxr && cnt < tne; i++)
         {
-            cout << arr[i][j];
+            cout << arr[i][j]<<endl;
+            cnt++;
         }
+        minc++;
+
+        // bottom wall 
+        for (int i = maxr,  j = minc; j <= maxc && cnt < tne; j++)
+        {
+            cout << arr[i][j]<<endl;
+            cnt++;
+        }
+        maxr--;
+
+        // right wall 
+        for (int i = maxr,  j = maxc; i >= minr && cnt < tne; i--)
+        {
+            cout << arr[i][j]<<endl;
+            cnt++;
+        }
+        maxc--;
+
+        // top wall 
+        for (int i = minr,  j = maxc; j >= minc && cnt < tne; j--)
+        {
+            cout << arr[i][j]<<endl;
+            cnt++;
+        }
+        minr++;
     }
+    
+}   
