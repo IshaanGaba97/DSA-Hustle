36ef674a5b37b7f9d595894878d014326414ba14
Ishaan Gaba <ishaangaba97@gmail.com>
2021-10-15 03:22:01 +0530

    Added subsets_of_array.cpp

---
+++ Level 1/Recursion and Backtracking/Factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    // write your code here
+    if(n == 1)
+    {
+        return 1;
+    }
+    int fnm = factorial(n-1); 
+    int fn = n * fnm;
+    return fn;
+}
+
+
+int main(){
+    int n; cin>>n;
+    cout<<factorial(n);
+}
---
+++ Level 1/Recursion and Backtracking/PowerLinear.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int power(int x, int n)
+{
+    // write your code here
+    if (n == 0)
+    {
+        return 1;
+    }
+    int pnm1 = power(x, n - 1);
+    int pn = pnm1 * x;
+    return pn;
+}
+
+int main()
+{
+    int n, x;
+    cin >> x >> n;
+    cout << power(x, n);
+}
