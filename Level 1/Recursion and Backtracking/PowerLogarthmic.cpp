3d67b3520ceda311cffb13f77ed4c795d9d39ae4
Ishaan Gaba <ishaangaba97@gmail.com>
2021-10-16 00:35:25 +0530

    added

---
+++ Level 1/Recursion and Backtracking/PowerLogarthmic.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int powerLogarithmic(int x,int n){
+    // write your code here
+    if(n == 0)
+    {
+        return 1;
+    }
+    int xpnb2 = powerLogarithmic(x, n/2);
+    int xn = xpnb2 * xpnb2;
+    if(n % 2 == 1)
+    {
+        xn = xn * x;
+    }
+    return xn;
+}
+
+int main(){
+    int x,n; cin>>x>>n;
+    cout<<powerLogarithmic(x,n);
+}
---
+++ Level 1/Recursion and Backtracking/PrintZigZag.cpp
@@ -0,0 +1,23 @@
+// 2 call question = solve with euler tree diagram
+
+#include<iostream>
+using namespace std;
+
+void pzz(int n){
+    if(n==0)
+    {
+        return;
+    }
+    cout << n << " ";
+    pzz(n-1);
+    cout << n << " ";
+    pzz(n-1);
+    cout << n << " ";
+}
+
+
+
+int main(){
+    int n; cin>>n;
+    pzz(n);
+}
