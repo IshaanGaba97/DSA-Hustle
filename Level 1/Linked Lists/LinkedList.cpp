c83bcf4e7d6c99bff3c0b16cdc1aaf0a69c08338
Ishaan Gaba <ishaangaba97@gmail.com>
2022-01-09 21:22:29 +0530

    added

---
+++ Level 1/Linked Lists/LinkedList.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+
+class LinkedList
+{
+    node *head;
+    node *tail;
+    int size;
+};
+
+int main()
+{
+    return 0;
+}
