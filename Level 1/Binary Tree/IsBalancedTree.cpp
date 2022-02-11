1b61790bd0f34d7355b0a62a076b3c4af9d51917
Ishaan Gaba <ishaangaba97@gmail.com>
2022-02-11 15:29:44 +0530

    added

---
+++ Level 1/Binary Tree/IsBalancedTree.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data=0;
+    Node *left = nullptr;
+    Node *right = nullptr;
+    Node(int data)
+    {
+        this->data = data;
+    }
+};
+
+ class Pair {
+    public:
+    Node *node=nullptr;
+    int state=0;
+
+    Pair(Node *node, int state) {
+      this->node = node;
+      this->state = state;
+    }
+  };
+
+int idx = 0;
+Node *constructTree(vector<int> &arr)
+{
+
+    if (idx == arr.size() || arr[idx] == -1)
+    {
+        idx++;
+        return nullptr;
+    }
+    Node *node = new Node(arr[idx++]);
+    node->left = constructTree(arr);
+    node->right = constructTree(arr);
+    return node;
+}
+
+//Display function
+void display(Node *node)
+{
+    if (node == nullptr)
+        return;
+    string str = "";
+    str += node->left != nullptr ? to_string(node->left->data) : ".";
+    str += " <- " + to_string(node->data) + " -> ";
+    str += node->right != nullptr ? to_string(node->right->data) : ".";
+    cout << str << endl;
+
+    display(node->left);
+    display(node->right);
+}
+
+
+//Height function
+int height(Node *node)
+{
+    return node == nullptr ? -1 : max(height(node->left), height(node->right)) + 1; 
+}
+
+bool isbal = true;
+ int isbalance(Node *node)
+  {
+    if(node == nullptr){
+      return 0;
+    }
+    int lh = isbalance(node->left);
+    int rh = isbalance(node->right);
+
+    int gap = abs(lh - rh);
+    if(gap > 1){
+      isbal = false;
+    }
+    int th = max(lh, rh) + 1;
+    return th;
+  }
+
+
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<int> arr(n,0);
+    for(int i = 0; i < n; i++) {
+        string tmp;
+        cin>>tmp;
+      if (tmp=="n") {
+        arr[i] = -1;
+      } else {
+        arr[i] = stoi(tmp);
+      }
+    }
+    
+    
+    Node *root = constructTree(arr);
+    
+    int r = isbalance(root);
+    if(isbal == 1)
+    cout << "true";
+    else 
+    cout<<"false";
+}
