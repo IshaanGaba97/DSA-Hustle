722d23069d4f01546d08f5ab5dad0669d3c4a5c8
Ishaan Gaba <ishaangaba97@gmail.com>
2022-02-14 16:11:38 +0530

    added

---
+++ Level 1/Basics/RotateANumber.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    
+    int temp = n;
+    int nod = 0;
+    
+    while(temp > 0)
+    {
+        temp = temp/10;
+        nod++;
+    }
+    
+    k = k % nod;
+    if(k < 0){
+        k = k + nod;
+    }
+    
+    int div = 1;
+    int mult = 1;
+    for(int i = 1; i <= nod; i++)
+    {
+        if(i <= k)
+        {
+            div = div * 10;
+        }
+        else
+        {
+            mult = mult * 10;
+        }
+    }
+    
+    int q = n/div;
+    int r = n%div;
+    
+    int rot = (r * mult) + q;
+    cout << rot;
+    
+    return 0;
+}
