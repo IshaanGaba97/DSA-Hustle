bf3b79481be48ee1c1ae630df49e877312f37555
Ishaan Gaba <ishaangaba97@gmail.com>
2022-02-15 20:55:41 +0530

    added

---
+++ Level 1/Hashmap and Heap/GetCommonElements-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cin >> n1;
+    int* arr1 = new int(n1);
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n1; i++)
+    {
+        cin >> arr1[i];
+        mp[arr1[i]]++;
+    }
+    cin >> n2;
+    int* arr2 = new int(n2);
+    for (int i = 0; i < n2; i++)
+    {
+        cin >> arr2[i];
+        if (mp.find(arr2[i]) != mp.end())
+        {
+            cout << arr2[i] << endl;
+            mp.erase(arr2[i]);
+        }
+    }
+}
