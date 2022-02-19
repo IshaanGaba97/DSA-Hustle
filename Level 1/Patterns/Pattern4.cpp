146f64bd9a259a1ea4c9d4cceccc9c751f9b8975
Ishaan Gaba <ishaangaba97@gmail.com>
2022-02-19 18:11:49 +0530

    added

---
+++ Level 1/Patterns/Pattern4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        {
+            for (int j = 1; j <= i - 1; j++)
+            {
+                cout << "	";
+            }
+            for (int j = 1 ; j <=  n - i + 1; j++)
+            {
+                cout << "*	";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
